31eb268a72a19a94c2ff8e2e5a551169ccb1df5a
giovanni <giovannifrozza470@gmail.com>
2020-11-05 11:06:58 -0300

    agora vai

---
+++ pacote-download/javascript/ex002/script.js
@@ -0,0 +1,8 @@
+window.alert('Minha primeira mensagem!')
+window.confirm('Esta gostando da js?')
+// window.prompt('qual seu nome?')
+
+var nome = window.prompt('Qual seu nome?')
+var sobrenome = window.prompt('Qual seu sobrenome?')
+window.alert(`Seja bem vindo, ${nome} ${sobrenome}!`)
+
