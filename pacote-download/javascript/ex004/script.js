1713a345c345d41f7bfcb680b317f409d4570007
giovanni <giovannifrozza470@gmail.com>
2020-11-05 11:02:58 -0300

    ex 004

---
+++ pacote-download/javascript/ex004/script.js
@@ -0,0 +1,6 @@
+var nome = window.prompt('Qual seu nomme?')
+document.write(`Seu nome tem ${nome.length} letras.</br>`)
+document.write(`Seu nome em maiusculo ${nome.toUpperCase()}</br>`)
+document.write(`Seu nome em maiusculo ${nome.toLowerCase()}</br>`)
+
+var num1 = 20;
