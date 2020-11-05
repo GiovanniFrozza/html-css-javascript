aca6f4593d05b5d067aa280183c1d6f2e000790b
giovanni <giovannifrozza470@gmail.com>
2020-11-05 11:04:04 -0300

    ex 003

---
+++ pacote-download/javascript/ex003/script.js
@@ -0,0 +1,8 @@
+window.alert('Minha primeira mensagem!')
+window.confirm('Esta gostando da js?')
+// window.prompt('qual seu nome?')
+
+var num1 = Number.parseFloat(window.prompt('Digite um numero'))
+var num2 = Number.parseFloat(window.prompt('Digite outro numero'))
+var soma = num1 + num2
+window.alert(`${num1} + ${num2} = ${soma}`)
