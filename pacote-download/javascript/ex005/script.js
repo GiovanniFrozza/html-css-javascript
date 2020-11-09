84f713396310b51a202b69f8e7fcb5a1a4f3cd43
giovanni <giovannifrozza470@gmail.com>
2020-11-09 14:32:50 -0300

    add o resto

---
+++ pacote-download/javascript/ex005/script.js
@@ -0,0 +1,6 @@
+var p1 = window.document.getElementsByTagName("p")[1]
+// var d = window.document.getElementById('mensagem')
+// d.innerText = 'Estou aguardando...'
+
+var d = window.document.querySelector('div.mensagem')
+d.style.background = "green"
